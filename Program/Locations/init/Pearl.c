632eb71ce76ba0701a5e6f25e6c9ca1ba72bc4fb
Sinistraa <Sireusss@yandex.ru>
2023-04-09 20:41:36 +0300

    1704b

--- Program/Locations/init/Pearl.c
+++ Program/Locations/init/Pearl.c
@@ -802,12 +802,17 @@ int LocationInitPearl(int n)
 	
 	locations[n].private1.key = "Peres_Kluch";
 	locations[n].private1.key.delItem = true;
-	locations[n].private1.items.toporAZ = 1;
-	locations[n].private1.items.blackbeard_sword_baron = 1;
 	locations[n].private1.items.food2 = 10;
 	locations[n].private1.items.food3 = 10;
+	locations[n].private1.items.pistol_grapebok = 1;
+	locations[n].private1.items.grapeshot = 25;
+	locations[n].private1.items.GunPowder = 25;
+	locations[n].private1.items.jewelry11 = 15;
+	locations[n].private1.items.jewelry12 = 40;
+	locations[n].private1.items.jewelry17 = 3;
 	locations[n].private1.items.CompCraft_PowderMixture = 10;
 	locations[n].private1.items.CompCraft_Sulfur = 10;
+	locations[n].private1.items.toporAZ = 1;
 	n = n + 1;
 
 
